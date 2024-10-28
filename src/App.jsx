520ca64993bdc3a953bf3fb1974bb20957e2d539
강동욱 <ehddnr8563@gmail.com>
2024-10-28 19:30:58 +0900

    chore: tailwind 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import ProjectsSidebar from './components/ProjectsSidebar';
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <main>
+      <ProjectsSidebar />
+    </main>
+  );
 }
 
 export default App;
