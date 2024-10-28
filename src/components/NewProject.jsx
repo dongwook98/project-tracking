6df237e109c9e6c55b7b9358643cd62309b2b7e8
강동욱 <ehddnr8563@gmail.com>
2024-10-29 01:22:45 +0900

    feat: NewProject 유효성 검사 기능 추가

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from 'react';
-import Input from './Input';
 
-export default function NewProject({ onAddProject }) {
+import Input from './Input';
+import Modal from './Modal';
+
+export default function NewProject({ onAddProject, onCancel }) {
+  const modalRef = useRef();
   const titleRef = useRef();
   const descriptionRef = useRef();
   const dueDateRef = useRef();
@@ -11,7 +14,14 @@ export default function NewProject({ onAddProject }) {
     const enteredDescription = descriptionRef.current.value;
     const enteredDueDate = dueDateRef.current.value;
 
-    // validation ...
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modalRef.current.open();
+      return;
+    }
 
     onAddProject({
       title: enteredTitle,
@@ -21,27 +31,43 @@ export default function NewProject({ onAddProject }) {
   }
 
   return (
-    <div className='w-[35rem] mt-16'>
-      <menu className='flex items-center justify-end gap-4 my-4'>
-        <li>
-          <button className='text-stone-800 hover:text-stone-950'>
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type='text' ref={titleRef} labelText='Title' />
-        <Input ref={descriptionRef} labelText='Description' isTextarea />
-        <Input type='date' ref={dueDateRef} labelText='Due Date' />
+    <>
+      <Modal ref={modalRef} closeButtonText='확인'>
+        <h2 className='text-xl font-bold text-stone-700 my-4'>
+          유효하지 않은 입력값 입니다.
+        </h2>
+        <p className='text-stone-600 mb-4'>
+          이런... 값 입력을 잊으신 것 같습니다.
+        </p>
+        <p className='text-stone-600 mb-4'>
+          모든 칸에 유효한 값을 입력하셨는지 확인해주세요.
+        </p>
+      </Modal>
+      <div className='w-[35rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4'>
+          <li>
+            <button
+              className='text-stone-800 hover:text-stone-950'
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type='text' ref={titleRef} labelText='Title' />
+          <Input ref={descriptionRef} labelText='Description' isTextarea />
+          <Input type='date' ref={dueDateRef} labelText='Due Date' />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
